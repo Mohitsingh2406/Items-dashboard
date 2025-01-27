86746c9838bddb66387d63efd950b23a16118f97
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-01-27 13:59:36 +0530

    Complete

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <HomeScreen/>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,64 @@
+import { SafeAreaView, StyleSheet, Text, View , Pressable } from 'react-native'
+import React, { useState } from 'react'
+import AllItems from './AllItems';
+import CreateScreen from './CreateScreen';
+
+const HomeScreen = () => {
+    const [view ,setView] = useState(0);
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Dashboard</Text>
+            <View style={styles.buttonContainer}>
+                <Pressable style={[styles.button, view===0? {backgroundColor:"green"} : null]} onPress={()=> setView(0)}>
+                    <Text style={[styles.btnText , view ===0 ? {color:"white"}:null]}>All Items</Text>
+                </Pressable>
+                <Pressable style={[styles.button, view===1? {backgroundColor:"green"} : null]} onPress={()=> setView(1)}>
+                    <Text style={[styles.btnText , view ===1 ? {color:"white"}:null]}>Low Stock</Text>
+                </Pressable>
+                <Pressable style={[styles.button, view===2? {backgroundColor:"green"} : null]} onPress={()=>setView(2)}>
+                    <Text style={[styles.btnText , view ===2 ? {color:"white"}:null]}>Create</Text>
+                </Pressable>
+            </View>
+
+            {view===0 && <AllItems/>}
+            {view===1 && <AllItems/>}
+            {view===2 && <CreateScreen/>}
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        padding:"4%",
+        backgroundColor:"#ffffff"
+    },
+
+    title:{
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#333",
+    },
+
+    buttonContainer:{
+        flexDirection:"row",
+        gap:10,
+        marginVertical:10
+    },
+
+    button:{
+        paddingVertical:3.5,
+        paddingHorizontal:10,
+        borderColor:"green",
+        borderRadius:50,
+        borderWidth:0.8,
+    },
+
+    btnText:{
+        fontSize:16,
+        color:"green",
+    }
+})
